7db620ca5c6c8be039ec54bafeb653695219487f
Sylwester Kulpa <sylwester.kulpa@gmail.com>
2016-02-13 17:56:31 +0100

    Add api response with error handling

--- livespace/tests.py
+++ livespace/tests.py
@@ -3,7 +3,7 @@
 import mock
 import unittest
 
-from livespace import Client
+from livespace import ApiError, Client
 
 
 class ClientTestCase(unittest.TestCase):
@@ -13,8 +13,22 @@ class ClientTestCase(unittest.TestCase):
         super(ClientTestCase, self).setUp()
 
     @mock.patch('requests.post')
-    def test_client_authorize(self, post):
-        pass
+    @mock.patch('livespace.Client.get_access_token')
+    def test_client_raises_api_error(self, get_access_token, post):
+        get_access_token.return_value = {
+            '_api_auth': 'key',
+            '_api_key': 'api key',
+            '_api_session': 'session',
+            '_api_sha': 'sha'}
+
+        class MyPost(mock.Mock):
+            def json(self):
+                return {'data': [], 'error': None, 'result': 561,
+                        'status': False}
+
+        post.return_value = MyPost()
+        with self.assertRaises(ApiError):
+            self.client('Default', 'ping', {'foo': 'bar'})
 
 
 if __name__ == '__main__':
